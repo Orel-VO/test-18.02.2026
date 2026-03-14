d38db7f1b0b84d57324d098180daf4a8f14e4d48
Orel-VO <st140793@student.spbu.ru>
2026-03-14 14:51:50 +0300

    Способ Б

---
+++ dz14.03/main_b.cpp
@@ -0,0 +1,8 @@
+#include "config_b.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << Config::kMax << "\n";       
+    const int* p = &Config::kMax;             
+    std::cout << *p << "\n";
+}
